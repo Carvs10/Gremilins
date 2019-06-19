d9bc2f461b80ba011e74417de809cfa73a066c69
Pedro Henrique <pedro.cardoso@ufrn.edu.br>
2019-06-19 20:16:22 -0300

    update

---
+++ mp.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio> //std::size_t
+
+
+
+
+class StoragePool{
+
+
+	public:
+
+		virtual ~StoragePool();
+		//operadores->
+
+		virtual void* Allocate( size_t ) = 0;
+		//gerar uma exceçao senao tiver a memoria solicitada
+
+		virtual void Free( void* ) = 0;
+
+
+
+	private:
+
+
+};
